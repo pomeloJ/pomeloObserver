7c5c2fdb467374dce7f61ea1f74ae82525468f37
pomeloJ <58825725+pomeloJ@users.noreply.github.com>
2019-12-18 03:21:39 +0800

    Update pobserver.js
    add some feature

--- pobserver.js
+++ pobserver.js
@@ -7,8 +7,8 @@ class pomeloObserver{
     constructor() {
         this.observerList = {}
     }
-    //register a task
-    register = function (event, name, func) {
+    //subscribe a task
+    subscribe = function (event, name, func) {
         if ((Object.keys(this.observerList)).indexOf(event) === -1){
             this.observerList[event] = {};
         }
@@ -19,10 +19,10 @@ class pomeloObserver{
             
         return true;
     }
-    //unregister a task
-    unregister = function (event, name) {
-        for (let func in this.observerList[event]) {
-            if (this.observerList[event].hasOwnProperty(func) && typeof this.observerList[event][name] === "function") {
+    //unsubscribe a task
+    unsubscribe = function (event, name) {
+        for (let funName in this.observerList[event]) {
+            if (this.observerList[event].hasOwnProperty(funName) && typeof this.observerList[event][name] === "function") {
                 delete this.observerList[name];
 
                 return true;
@@ -37,19 +37,69 @@ class pomeloObserver{
             return false;
         }
         let i = 0;
-        for (let func in this.observerList[event]) {
-            if (this.observerList[event].hasOwnProperty(func)){
-                this.observerList[event][func](data);
+        for (let funName in this.observerList[event]) {
+            if (this.observerList[event].hasOwnProperty(funName)){
+                this.observerList[event][funName](data);
                 i++;
             }
         }
 
         return i;
     }
+    //trigger in filter
+    triggerIn = function (event,filt,data) {
+        if (typeof this.observerList[event] === "undefined") {
+            return false;
+        }
+        
+        let i = 0;
+        for (let funName in this.observerList[event]) {
+            let inArrayResult = filt.find(function (item, index, array) {
+                return item === funName;
+            });
+
+            if (this.observerList[event].hasOwnProperty(funName) && inArrayResult != null) {
+                this.observerList[event][funName](data);
+                i++;
+            }
+        }
+
+        return i;
+    }
+    //trigger not in filter
+    triggerNotIn = function (event,filt,data) {
+        if (typeof this.observerList[event] === "undefined") {
+            return false;
+        }
+        
+        let i = 0;
+        for (let funName in this.observerList[event]) {
+            let inArrayResult = filt.find(function (item, index, array) {
+                return item === funName;
+            });
+
+            if (this.observerList[event].hasOwnProperty(funName) && inArrayResult == null) {
+                this.observerList[event][funName](data);
+                i++;
+            }
+        }
+
+        return i;
+    }
+    //action a event unit
+    action = function (event,name,data){
+        if (typeof this.observerList[event][name] === "undefined") {
+            return false;
+        }
+
+        this.observerList[event][name](data);
+
+        return true;
+    }
     //debug
     debug = function(){
         let list = this.observerList;
-        
+
         let result = {}
         let index = Object.keys(this.observerList);
 
